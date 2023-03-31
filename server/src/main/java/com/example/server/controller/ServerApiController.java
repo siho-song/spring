4b6c6bdbfdc389947c7d7ad6b655c125953b3c63
siho-song <ssh_1678@naver.com>
2023-03-31 12:08:42 +0900

    "studied"

---
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -0,0 +1,27 @@
+package com.example.server.controller;
+
+
+import com.example.server.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/server")
+public class ServerApiController {
+
+    @GetMapping("/hello")
+    public User hello(@RequestParam String name, @RequestParam int age){
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        return user;
+    }
+
+    @PostMapping("/server/user/{userId}/name/{userName}")
+    public User post(@RequestBody User user,@PathVariable int userId, @PathVariable String userName){
+        log.info("userId : {} , userName : {}",userId,userName);
+        log.info("client req : {}",user);
+        return user;
+    }
+}
