be7c7a6196e551effaf8bf89d85b376fb77bf3af
siho-song <ssh_1678@naver.com>
2023-04-02 01:47:24 +0900

    "Unit test studied"

---
+++ spring-calculator/src/main/java/com/example/springcalculator/component/Calculator.java
@@ -0,0 +1,20 @@
+package com.example.springcalculator.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class Calculator {
+    public final ICalculator iCalculator;
+    public int sum(int x, int y){
+        this.iCalculator.init();
+        return this.iCalculator.sum(x,y);
+    }
+
+    public int minus(int x, int y){
+        this.iCalculator.init();
+        return this.iCalculator.minus(x,y);
+    }
+}
---
+++ spring-calculator/src/main/java/com/example/springcalculator/controller/CalculatorApiController.java
@@ -0,0 +1,51 @@
+package com.example.springcalculator.controller;
+
+import com.example.springcalculator.component.Calculator;
+import com.example.springcalculator.dto.Req;
+import com.example.springcalculator.dto.Res;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CalculatorApiController {
+
+    private final Calculator calculator;
+
+//    @GetMapping("/sum")
+//    public int sum(@RequestParam int x, @RequestParam int y){
+//        return calculator.sum(x,y);
+//    }
+//
+//
+//    @GetMapping("/minus")
+//    public int minus(@RequestParam int x, @RequestParam int y){
+//        return calculator.minus(x,y);
+//    }
+
+
+
+    @PostMapping ("/sum")
+    public Res sum(@RequestBody Req req){
+        int result = calculator.sum(req.getX(), req.getY());
+
+        Res res = new Res();
+        res.setResult(result);
+
+        return res;
+    }
+
+
+    @PostMapping("/minus")
+    public Res minus(@RequestBody Req req){
+
+        int result = calculator.minus(req.getX(), req.getY());
+
+        Res res = new Res();
+        res.setResult(result);
+        res.setResponse(new Res.Body());
+
+        return res;
+    }
+}
