2ae0358c6ec772133ce2ecac08cb4004b6cc53b0
siho-song <ssh_1678@naver.com>
2023-04-01 23:30:34 +0900

    "JUnit Test studied"

--- calculator/src/test/java/DollarCalculatorTest.java
+++ calculator/src/test/java/DollarCalculatorTest.java
@@ -2,10 +2,24 @@ import org.example.Calculator;
 import org.example.DollarCalculator;
 import org.example.MarketApi;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DollarCalculatorTest {
 
+    @Mock
+    public MarketApi marketApi;
+
+    @BeforeEach
+    public void init(){
+        Mockito.lenient().when(marketApi.connect()).thenReturn(3000);
+
+    }
     @Test
     public void testHello(){
         System.out.println("Hello");
@@ -18,6 +32,21 @@ public class DollarCalculatorTest {
         System.out.println(calculator.sum(10,10));
 
         Assertions.assertEquals(22000, calculator.sum(10,10));
+        Assertions.assertEquals(0,calculator.minus(10,10));
+    }
+
+
+    @Test
+    public void mockTest(){
+        System.out.println("Hello");
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        System.out.println(calculator.sum(10,10));
+
+        Assertions.assertEquals(60000, calculator.sum(10,10));
         Assertions.assertEquals(0,calculator.minus(10,10));
     }
 
